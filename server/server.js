ecc74d44f7c75cd34d9cfa621335dcb3837fa1c2
Nosieek <mateusz.nosko13@gmail.com>
2024-03-07 19:19:52 +0100

    Added CORS support

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser');
 const calculateRouter = require('./calculate')
+const cors = require('cors')
+const app = express()
+app.use(cors());
+var corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+}
 
 app.use(bodyParser.json());
-app.use('/api', calculateRouter);
+app.use('/api',cors(corsOptions), calculateRouter);
+
 
 const PORT = 4848
 app.listen(PORT, ()=> console.log(`Server started on the port 4848: http://localhost:${PORT}`))
